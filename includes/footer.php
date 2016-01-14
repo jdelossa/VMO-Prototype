4a852799dedcb1383d1797bf53a9b73987ef6cc2
Joanna De Los Santos <jdelossa93@gmail.com>
2016-01-14 17:43:11 -0500

    started new header, incomplete

---
+++ includes/footer.php
@@ -0,0 +1,63 @@
+<footer>
+        <div id="contact" class="container">
+            <!-- Contact -->
+
+            <div class="row">
+                <div class="col-sm-12">
+                    <h3>Contact Us</h3><hr>
+                </div>
+            </div>
+
+            <div  class="row">
+                <div class="col-sm-8">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Huis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <br>
+                    <p><strong>Address: </strong>Winthrop-University Hospital <br>
+                        259 First Street <br>
+                        Mineola NY 11501
+                    </p>
+                    <p><strong>Phone: </strong><a href="#">516-663-1756</a></p>
+                    <p><strong>Email: </strong><a href="#">vmo@winthrop.org</a></p>
+                </div>
+
+                <div class="col-sm-4">
+                    <form>
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="name-input" placeholder="Your Name *">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="email-input" placeholder="Email *">
+                        </div>
+
+                        <div class="form-group">
+                            <select id="reason"class="form-control">
+                              <option>I want to join</option>
+                              <option>I have a question</option>
+                              <option>I have a concern</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <textarea class="form-control" id="comments" placeholder="Comment" rows="3"></textarea>
+                        </div>
+
+
+                          <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+                    <br>
+                    <br>
+                </div>
+            </div>
+            <hr>
+            <p class="pull-left">&copy; Winthrop-University Hospital 2015</p>
+            <p class="pull-right" style="text-decoration: underline;"> Privacy Policy</p>
+        </div>
+    </footer>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+    <script src="js/vendor/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+
+    </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Medical Staff Portal | Home</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="../js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+        <script src="../js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="../js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <!-- Calendar -->
+
+        <script type="text/javascript">
+            $(document).ready(function() {
+                // page is now ready, initialize the calendar...
+                $('#calendar').fullCalendar({
+                    eventSources: [
+                        {
+                            events: [
+                                {
+                                    title : 'Holiday Fundraising',
+                                    start : '2015-12-18'
+
+                                },
+                                {
+                                    title : 'Event Title',
+                                    start : '2015-12-19'
+
+                                },
+                                {
+                                    title : 'Event Title',
+                                    start : '2015-12-20'
+
+                                },
+                                {
+                                    title : 'Event Title',
+                                    start : '2015-12-21'
+
+                                },
+                                {
+                                    title : 'Event Title',
+                                    start : '2015-12-22'
+
+                                },
+                                {
+                                    title : 'Event Title',
+                                    start : '2015-12-24'
+
+                                }
+
+                            ],
+                            color: '#3E7698',
+                            textColor: '#ffffff'
+                        }
+                    ]
+                });
+            });
+        </script>
+
+        <link rel="stylesheet" type="text/css" href="../bower_components/fullcalendar/dist/fullcalendar.min.css">
+        <script src="../bower_components/jquery/dist/jquery.min.js"></script>
+        <script src="../bower_components/moment/min/moment.min.js"></script>
+        <script src="../bower_components/fullcalendar/dist/fullcalendar.min.js"></script>
+    </head>
+
+    <body id="home">
+            <!--[if lt IE 8]>
+                <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+            <![endif]-->
+        <div class="status-bar"></div>
+        <!-- Nav -->
+        <nav class="navbar navbar-fixed-top">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="navbar-header">
+                            <a class="navbar-brand" href="index.php"><img class="logo"src="images/logo.png" alt="Winthrop Logo">
+                                <span class="title">Winthrop Medical Staff</span></a>
+                        </div>
+                        <div class="header-btn pull-right">
+                            <a href="#" class="btn btn-default find-doctor" role="button">Find A Doctor</a>
+                            <a href="#" class="btn btn-default member-login" role="button">Member Login</a>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-12">
+                        <ul class="nav navbar-nav">
+                            <li><a href="#about" class="active">Home</a></li>
+                            <li><a href="#">Membership</a></li>
+                            <li><a href="#">Leadership</a></li>
+                            <li><a href="events.php">Events</a></li>
+                            <li><a href="news.php">News</a></li>
+                            <li><a href="#contact">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+            <div style="border-bottom: solid 1px #3E7698;"></div>
+        </nav>
+        <!-- End Nav -->
